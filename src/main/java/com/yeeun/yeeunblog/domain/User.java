89454171c526feabdad0a4f0194964f7d92b6d3a
dhrxh99 <dhrxhan@gmail.com>
2025-08-21 18:12:17 +0900

    작성자 Entity 1차 생성

---
+++ src/main/java/com/yeeun/yeeunblog/domain/User.java
@@ -0,0 +1,24 @@
+package com.yeeun.yeeunblog.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private char password;
+}
